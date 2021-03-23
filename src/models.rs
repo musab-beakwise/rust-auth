5847d1da04b8c5473cd0394304f87bb4ced103be
merayman <musab.erayman@gmail.com>
2021-03-23 11:39:17 +0300

    Example diesel actix postgres restfull app

---
+++ src/models.rs
@@ -0,0 +1,24 @@
+// src/models.rs
+
+use crate::schema::*;
+use serde::{Deserialize, Serialize};
+
+//exact data
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+pub struct User {
+    pub id: i32,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub created_at: chrono::NaiveDateTime,
+}
+
+//input to database
+#[derive(Insertable, Debug)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+    pub first_name: &'a str,
+    pub last_name: &'a str,
+    pub email: &'a str,
+    pub created_at: chrono::NaiveDateTime,
+}
